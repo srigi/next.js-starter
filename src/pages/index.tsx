86e876edb6945f73018a3ce19f6cc8dad7520051
srigi <srigi@srigi.sk>
2020-10-13 21:23:05 +0200

    Add <Nav> component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from 'next';
 import Head from 'next/head';
 import React from 'react';
 
+import Nav from '../components/Nav';
+
 const IndexPage: NextPage = () => {
   return (
     <>
@@ -9,6 +11,7 @@ const IndexPage: NextPage = () => {
         <title>Welcome · Next.js starter</title>
       </Head>
 
+      <Nav />
       <main className="py-20">
         <h1 className="text-5xl text-center text-accent-1">Next.js starter</h1>
       </main>
