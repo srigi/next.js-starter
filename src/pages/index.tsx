e6c3709fe4ebb0490f48130e80f8d4939f74349f
srigi <srigi@srigi.sk>
2020-10-15 13:18:19 +0200

    Add some basic content to IndexPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import { NextPage } from 'next';
 import React from 'react';
+import cn from 'classnames';
 
 import SimpleLayout from '../components/layouts/SimpleLayout';
+import styles from './index.module.css';
 
 const IndexPage: NextPage = () => {
   return (
     <SimpleLayout title="Welcome">
-      <h1 className="font-thin text-5xl text-center mb-2">Next.js starter</h1>
-      <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <header className={cn(styles.header, 'mb-4')}>
+        <div className={cn(styles.container, 'md:px-4')}>
+          <section className={styles.headerContent}>
+            <h1 className="font-thin text-5xl mb-2">Next.js starter</h1>
+            <p>Jump right into most successful ReactJS framework</p>
+          </section>
+        </div>
+      </header>
+
+      <section className={cn(styles.container, 'px-4')}>
+        <p>Highly pimped dev-stack for Next.js</p>
+        <p>Very strict code-quality tools included</p>
+        <p>Develop backend &amp; frontend together</p>
+      </section>
     </SimpleLayout>
   );
 };
