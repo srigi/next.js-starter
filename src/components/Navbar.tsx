f01ce9238e036ec37a05218a3593b279d83f075e
srigi <srigi@srigi.sk>
2020-11-04 11:13:56 +0100

    Cleanup the Navbar brand logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,20 +8,18 @@ const { NEXT_PUBLIC_SITE_NAME: siteName } = process.env;
 const Navbar: FunctionComponent = () => {
   return (
     <BootstrapNavbar className="pr-0 pl-0" variant="light" expand="md">
-      <BootstrapNavbar.Brand>
-        <Link href="/">
-          <a className="navbar-brand">
-            <img
-              className="d-inline-block align-top mr-2"
-              width={30}
-              height={30}
-              src="/android-chrome-512x512.png"
-              alt="go to home link"
-            />
-            {siteName}
-          </a>
-        </Link>
-      </BootstrapNavbar.Brand>
+      <Link href="/">
+        <a className="navbar-brand">
+          <img
+            alt="Project logo"
+            className="d-inline-block align-top"
+            src="/android-chrome-512x512.png"
+            width={30}
+            height={30}
+          />{' '}
+          {siteName}
+        </a>
+      </Link>
 
       <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
       <BootstrapNavbar.Collapse id="basic-navbar-nav">
