c04896b5c88474bac2639a45a1025ccec6cd85cf
srigi <srigi@srigi.sk>
2020-11-15 17:27:38 +0100

    Check if JSON from DB is already parsed or type of string

--- src/pages/db.tsx
+++ src/pages/db.tsx
@@ -60,7 +60,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
       users: users.map((user) => ({
         id: user.id,
         userName: user.user_name,
-        ...(user.roles != null && { roles: JSON.parse(user.roles) }),
+        ...(user.roles != null && { roles: typeof user.roles === 'string' ? JSON.parse(user.roles) : user.roles }),
         ...(user.created_at != null && {
           createdAt: format(new Date(user.created_at), '{yyyy}-{MM}-{dd}, {HH}:{mm}:{ss}'),
         }),
