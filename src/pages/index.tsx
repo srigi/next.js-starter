21919a23c0029267ca65ec087236277e305999f5
srigi <srigi@srigi.sk>
2020-11-15 10:36:31 +0100

    Style <header> of index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
-import { Container, Col, Row } from 'react-bootstrap';
-import cn from 'classnames';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 import SimpleLayout from '../components/layouts/SimpleLayout';
 import styles from './index.module.css';
@@ -8,15 +9,13 @@ import styles from './index.module.css';
 const IndexPage: NextPage = () => {
   return (
     <SimpleLayout title="Welcome">
-      <header className={cn(styles.header, 'mb-4')}>
-        <Container className="mw-800px">
-          <Row className="px-md-3">
-            <Col className={styles.headerContent}>
-              <h1 className="font-thin text-5xl mb-2">Next.js starter</h1>
-              <p>Jump right into most successful ReactJS framework</p>
-            </Col>
-          </Row>
-        </Container>
+      <header className={styles.header}>
+        <div className={styles.headerHero}>
+          <Container>
+            <h1>Next.js starter</h1>
+            <p>Jump right into most successful ReactJS framework</p>
+          </Container>
+        </div>
       </header>
 
       <Container className="mw-800px">
