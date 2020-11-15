bb5c35842959d966cb7e6228329c0d6c96f540d1
srigi <srigi@srigi.sk>
2020-11-15 17:29:27 +0100

    Setup Dokku proxy mapping conditionally
    ...if DOKKU_APP_PORT is present.
    
    Also add some nice color for success messages

--- .dokku/10_create-webapp.sh
+++ .dokku/10_create-webapp.sh
@@ -9,17 +9,19 @@ DOKKU_HOSTS=$(compgen -A variable | grep DOKKU_HOST_)
 for DOKKU_HOST in ${DOKKU_HOSTS}; do
 	printf "${COLOR_YELLOW}Creating${NC} Dokku app ${COLOR_BLUE}${DOKKU_APP_NAME}${NC} at Dokku host ${COLOR_GREEN}${!DOKKU_HOST}${NC}... "
 	ssh ${!DOKKU_HOST} dokku apps:create ${DOKKU_APP_NAME} 2>/dev/null
-	echo "OK!"
+	echo -e "${COLOR_GREEN}OK${NC}"
 done
 
 for DOKKU_HOST in ${DOKKU_HOSTS}; do
 	printf "${COLOR_YELLOW}Setting-up${NC} Git remote ${COLOR_BLUE}dokku@${!DOKKU_HOST}${NC}... "
 	git remote add dokku@${!DOKKU_HOST} dokku@${!DOKKU_HOST}:${DOKKU_APP_NAME} 2>/dev/null
-	echo "OK!"
+	echo -e "${COLOR_GREEN}OK${NC}"
 done
 
-for DOKKU_HOST in ${DOKKU_HOSTS}; do
-	printf "${COLOR_YELLOW}Configuring${NC} port-mapping for Dokku app ${COLOR_BLUE}${DOKKU_APP_NAME}${NC} at Dokku host ${COLOR_GREEN}${!DOKKU_HOST}${NC}... "
-	ssh ${!DOKKU_HOST} dokku proxy:ports-set ${DOKKU_APP_NAME} http:80:3000
-	echo "OK!"
-done
+if [[ -n ${DOKKU_APP_PORT} ]]; then
+	for DOKKU_HOST in ${DOKKU_HOSTS}; do
+		printf "${COLOR_YELLOW}Configuring${NC} port-mapping for Dokku app ${COLOR_BLUE}${DOKKU_APP_NAME}${NC} at Dokku host ${COLOR_GREEN}${!DOKKU_HOST}${NC}...\n"
+		ssh ${!DOKKU_HOST} dokku proxy:ports-set ${DOKKU_APP_NAME} http:80:${DOKKU_APP_PORT}
+		echo -e "       ${COLOR_GREEN}Done${NC}"
+	done
+fi
