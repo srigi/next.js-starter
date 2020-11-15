c86961609b921119080ac4014155359f0d608954
srigi <srigi@srigi.sk>
2020-11-15 11:25:15 +0100

    Add styling for content & footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,15 +18,55 @@ const IndexPage: NextPage = () => {
         </div>
       </header>
 
-      <Container className="mw-800px">
-        <Row>
-          <Col>
-            <p>Highly pimped dev-stack for Next.js</p>
-            <p>Very strict code-quality tools included</p>
-            <p>Develop backend &amp; frontend together</p>
-          </Col>
-        </Row>
-      </Container>
+      <section className={styles.section}>
+        <Container>
+          <Row>
+            <Col>
+              <div className={styles.shoutText}>
+                <h3>Typescript only</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste
+                  esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum
+                  molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </Col>
+            <Col className="text-center justify-content-center align-self-center">
+              <div className={styles.shoutImage}>
+                <img src="/img/logo-TS.png" alt="Typescript logo" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className={styles.section}>
+        <Container>
+          <Row>
+            <Col className="text-center justify-content-center align-self-center">
+              <div className={styles.shoutImage}>
+                <img src="/img/logo-ESLint.png" alt="ESLint logo" />
+              </div>
+            </Col>
+            <Col>
+              <div className={styles.shoutText}>
+                <h3>Maximum strictness to code-quality</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste
+                  esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum
+                  molestiae adipisci, beatae obcaecati.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <footer className={styles.footer}>
+        <Container>
+          <p>Copyright &copy; 2020</p>
+        </Container>
+      </footer>
     </SimpleLayout>
   );
 };
