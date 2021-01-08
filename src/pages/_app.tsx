176b3343d5f9e2d6929755ebd902fcbd5cda3d10
srigi <srigi@srigi.sk>
2021-01-08 16:24:06 +0100

    Install Material UI libs. & prepare integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,37 @@
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import { AppProps } from 'next/app';
-import { StrictMode, FunctionComponent } from 'react';
-import sentry from '../lib/sentry';
+import { StrictMode, FunctionComponent, useEffect } from 'react';
 
-import './_app.css';
+import sentry from '../lib/sentry';
+import theme from '../theme';
 
 sentry.init();
 
-const App: FunctionComponent<AppProps & { err: Record<string, unknown> }> = ({ Component, pageProps, err }) => (
-  <StrictMode>
-    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-    <Component {...pageProps} err={err} />
-  </StrictMode>
-);
+export const cache = createCache({ key: 'css', prepend: true });
+
+const App: FunctionComponent<AppProps & { err: Record<string, unknown> }> = ({ Component, pageProps, err }) => {
+  // Remove the server-side CSS
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles != null) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StrictMode>
+      <CacheProvider value={cache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} err={err} />
+        </ThemeProvider>
+      </CacheProvider>
+    </StrictMode>
+  );
+};
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,14 @@
+import createEmotionServer from '@emotion/server/create-instance';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Children } from 'react';
 
-class MyDocument extends Document {
+import { cache } from './_app';
+import theme from '../theme';
+
+const { extractCritical } = createEmotionServer(cache);
+
+export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html>
@@ -12,7 +20,7 @@ class MyDocument extends Document {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#2d89ef" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="theme-color" content={theme.palette.primary.main} />
 
           {/* webfont(s) */}
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -45,4 +53,51 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument;
+// On the server:
+//   1. app.getInitialProps
+//   2. page.getInitialProps
+//   3. document.getInitialProps
+//   4. app.render
+//   5. page.render
+//   6. document.render
+//
+// On the server with error:
+//   1. document.getInitialProps
+//   2. app.render
+//   3. page.render
+//   4. document.render
+//
+// On the client
+//   1. app.getInitialProps
+//   2. page.getInitialProps
+//   3. app.render
+//   4. page.render
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const styles = extractCritical(initialProps.html);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+      <style
+        key="emotion-style-tag"
+        data-emotion={`css ${styles.ids.join(' ')}`}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: styles.css }}
+      />,
+    ],
+  };
+};
