c8dc8519e0c94987408be585790f678da4474f07
srigi <srigi@srigi.sk>
2021-01-09 19:35:01 +0100

    Adapt Index page to Material UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,61 @@
-import classNames from 'classnames';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { cyan } from '@material-ui/core/colors';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import Link from 'next/link';
 import { FunctionComponent } from 'react';
-import Button from 'react-bootstrap/Button';
-import BootstrapNavbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { FiExternalLink } from 'react-icons/fi';
-
-import styles from './Navbar.module.css';
 
 const { NEXT_PUBLIC_SITE_NAME: siteName } = process.env;
 
-const Navbar: FunctionComponent = () => {
-  return (
-    <BootstrapNavbar className={styles.navbar} bg="dark" variant="dark" expand="md">
-      <Link href="/">
-        <a className={classNames(styles.brand, 'navbar-brand')}>
-          <img alt="Project logo" className="d-inline-block align-top" src="/img/logo.png" width={30} height={30} />{' '}
-          {siteName}
-        </a>
-      </Link>
+const useStyles = makeStyles({
+  brand: {
+    alignItems: 'center',
+    color: cyan[100],
+    display: 'flex',
+    marginRight: 16,
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+  },
+  brandImage: {
+    marginRight: 8,
+  },
+  brandText: {
+    fontSize: '1.25rem',
+  },
+  flexibleSpacer: {
+    flexGrow: 1,
+  },
+});
 
-      <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
-      <BootstrapNavbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <BootstrapNavbar.Text>
-            <Button className="text-white" href="https://github.com/srigi/next.js-starter" target="_blank">
-              Github <FiExternalLink className="inline-block" />
-            </Button>
-          </BootstrapNavbar.Text>
-        </Nav>
-      </BootstrapNavbar.Collapse>
-    </BootstrapNavbar>
+const Navbar: FunctionComponent = () => {
+  const styles = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Link href="/">
+          <a className={styles.brand}>
+            <img alt="Project logo" className={styles.brandImage} src="/img/logo.png" width={30} />{' '}
+            <Typography component="span" className={styles.brandText}>
+              {siteName}
+            </Typography>
+          </a>
+        </Link>
+        <div className={styles.flexibleSpacer} />
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<GitHubIcon />}
+          href="https://github.com/srigi/next.js-starter"
+          target="_blank"
+        >
+          Github
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 };
 
