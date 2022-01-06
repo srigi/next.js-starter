8a45952ef13539a46965f64e1a458eaf7c54462f
Srigi <srigi@srigi.dev>
2022-01-06 16:31:56 +0100

    Set website-name via ENV var

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const { WEBSITE_NAME } = process.env;
+process.env.NEXT_PUBLIC_WEBSITE_NAME = WEBSITE_NAME;
+
 module.exports = {
   reactStrictMode: true,
 };
