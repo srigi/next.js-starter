37dc3d11ef29957930bc6756ebe2b4b2a4acec2c
Srigi <srigi@srigi.dev>
2022-01-06 17:33:00 +0100

    Add cutom font (self-served)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,15 @@ export default class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+
+          {/* webfont(s) */}
+          <link
+            rel="preload"
+            href="/fonts/alata-v2-latin-ext_latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
